2fb650e3d8b154bf84e443c076b92e772f0332b0
lwawrowski <lukasz8989@gmail.com>
2023-11-18 22:08:14 +0100

    ppr 20231118

---
+++ podstawy_programowania/2023_2024/ppr_20231118_typy_danych.R
@@ -0,0 +1,32 @@
+# mój pierwszy komentarz
+
+6/2*(2+1)
+
+# różne funkcje ----
+
+Sys.sleep(time = 3)
+
+sqrt(x = 100)
+sqrt(100)
+
+log(x = 27, base = 3)
+log(base = 3, x = 27)
+log(27, 3)
+log(3, 27) # źle
+
+# obiekty ----
+
+0/0 # NaN
+
+3/0 # Inf
+
+liczba <- 5
+liczba
+
+liczba_pi <- pi
+
+pi <- 5
+rm(pi)
+
+letters
+
