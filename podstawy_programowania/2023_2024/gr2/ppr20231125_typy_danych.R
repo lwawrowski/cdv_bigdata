035a7aad5c2bcd47074b59bfb0b698bcf7a4c71b
lwawrowski <lukasz8989@gmail.com>
2023-11-25 12:19:47 +0100

    ppr 20231125

---
+++ podstawy_programowania/2023_2024/gr2/ppr20231125_typy_danych.R
@@ -0,0 +1,52 @@
+# mój pierwszy komentarz
+
+6/2*(2+1)
+
+Sys.sleep(time = 10)
+
+sqrt(x = 100)
+sqrt(100)
+
+log(x = 27, base = 3)
+log(27, 3)
+log(base = 3, x = 27)
+log(3, 27) # źle
+
+# wartości ----
+
+0/0 # NaN
+
+3/0 # Inf
+
+pi*10
+
+liczba_pi <- 5
+liczba_pi
+
+pi <- 5
+pi
+
+rm(pi)
+pi
+
+letters
+
+month.name
+
+# wektory ----
+
+oceny <- c(4,5,4,3,5,6,3.5)
+
+mean(x = oceny)
+length(x = oceny)
+
+samochod <- c(TRUE, TRUE, FALSE, TRUE, FALSE)
+samochod <- c(True, True) # źle
+samochod <- c(T, T, F, T, F)
+
+ksiazki <- c("Lalka", "Pan Tadeusz", "Chłopi", 'Nędznicy')
+ksiazki
+
+mieszanka <- c(4, TRUE, "Lalka")
+mieszanka2 <- c(4, TRUE)
+mieszanka3 <- c('4', 4)
