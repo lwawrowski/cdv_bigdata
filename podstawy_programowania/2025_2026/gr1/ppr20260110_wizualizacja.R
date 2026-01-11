b8304e26bbd8fec22ea13796a07768b45e2fdc9f
lwawrowski <lukasz8989@gmail.com>
2026-01-11 16:43:10 +0100

    ppr 20260111

--- podstawy_programowania/2025_2026/gr1/ppr20260110_wizualizacja.R
+++ podstawy_programowania/2025_2026/gr1/ppr20260110_wizualizacja.R
@@ -13,7 +13,8 @@ hist(sklep$sprzedaz)
 
 ggplot(data = sklep, mapping = aes(x = liczba_klientow,
                                    y = sprzedaz, 
-                                   color = czy_promocja)) +
+                                   color = czy_promocja,
+                                   label = data)) +
   geom_point() +
   xlab("Liczba klientów") +
   ylab("Sprzedaż") +
@@ -23,4 +24,92 @@ ggplot(data = sklep, mapping = aes(x = liczba_klientow,
   labs(caption = "opracowanie własne") +
   theme_light() +
   theme(legend.position = "inside", legend.position.inside = c(0.9, 0.25))
-  
+
+ggsave(filename = "figs/sklep10_ggplot.png", width = 8, height = 4)
+ggsave(filename = "figs/sklep10_ggplot.pdf", device = cairo_pdf)
+
+# wykres interaktywny
+
+plotly::ggplotly()
+
+# zadanie
+
+temp_maj <- read_csv("data/temp_maj.csv")
+
+ggplot(temp_maj, aes(x = lon, y = lat, color = temp)) +
+  geom_point() +
+  scale_color_gradient2(name = "Temperatura", low = "blue", mid = "white", high = "red") +
+  theme_void() +
+  theme(legend.position = "top") +
+  coord_fixed()
+
+# histogram
+
+ggplot(sklep, aes(x = sprzedaz)) +
+  geom_histogram()
+
+ggplot(sklep, aes(x = sprzedaz)) +
+  geom_histogram(bins = 12)
+
+p <- ggplot(sklep, aes(x = sprzedaz)) +
+  geom_histogram(binwidth = 500)
+
+przedzialy <- layer_data(p)
+
+ggplot(sklep, aes(x = sprzedaz, fill = czy_promocja)) +
+  geom_histogram(binwidth = 500)
+
+ggplot(sklep, aes(x = sprzedaz)) +
+  geom_histogram(binwidth = 500, fill = "pink", color = "white")
+
+# wykres słupkowy
+
+# dane jednostkowe
+ggplot(sklep, aes(x = czy_promocja)) +
+  geom_bar()
+
+# dane zagregowane
+sklep_promocja <- sklep %>% 
+  count(czy_promocja)
+
+ggplot(sklep_promocja, aes(x = czy_promocja, y = n)) +
+  geom_col()
+
+sklep %>% 
+  count(czy_promocja) %>% 
+  ggplot(aes(x = czy_promocja, y = n)) +
+  geom_col()
+
+# druga cecha
+ggplot(sklep, aes(x = czy_promocja, fill = czy_swieto_szkolne)) +
+  geom_bar(position = "dodge")
+
+# wykres pudełkowy
+
+ggplot(sklep, aes(x=sprzedaz, y = czy_promocja)) +
+  geom_boxplot()
+
+ggplot(sklep, aes(y=sprzedaz, x = czy_promocja)) +
+  geom_boxplot()
+
+ggplot(sklep, aes(x=sprzedaz, y = czy_promocja)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.2, color = "blue")
+
+ggplot(sklep, aes(x=sprzedaz, y = czy_promocja)) +
+  geom_jitter(alpha = 0.2, color = "blue") +
+  geom_boxplot() 
+
+# zadanie
+
+ggplot(sklep, aes(x=liczba_klientow, y=as.character(dzien_tyg))) +
+  geom_boxplot()
+
+ggplot(sklep, aes(x=liczba_klientow, y=as.factor(dzien_tyg))) +
+  geom_boxplot()
+
+
+
+
+
+
---
+++ podstawy_programowania/2025_2026/gr1/ppr20260111_testy_statystyczne.R
@@ -0,0 +1,131 @@
+library(tidyverse)
+
+smog <- read_csv2("data/smog_pszczyna.csv")
+
+smog <- smog %>% 
+  mutate(pm10_poziom=if_else(condition = pm10 < 50, true = "w normie", false = "podwyższony"),
+         tmin_zero=if_else(tmin_daily < 0, "poniżej 0", "powyżej 0"))
+
+ggplot(data = smog, mapping = aes(x = tmax_daily, y = tmin_daily)) +
+  geom_point()
+
+smog2017 <- smog %>%  
+  filter(rok == 2017)
+
+# test niezależności 
+
+# H0: poziom pm10 i kategoria temperatury są niezależne
+# H1: poziom pm10 i kategoria temperatury nie są niezależne
+
+# p = 95%
+# alfa = 0.05
+
+table(smog$pm10_poziom, smog$tmin_zero)
+chisq.test(table(smog$pm10_poziom, smog$tmin_zero))
+
+# wartość p < alfa = 0.05 - odrzucamy hipotezę zerową
+# poziom pm10 i kategoria temperatury nie są niezależne
+
+# test proporcji
+
+# H0: odsetek palaczy w obu grupach jest taki sam
+# H1: odsetek palaczy w obu grupach nie jest taki sam
+
+# p = 95%
+# alfa = 0.05
+
+prop.test(x = c(200, 250), n = c(500, 600))
+
+# wartość p > alfa = 0.05 - nie mamy podstaw do odrzucenia H0
+# odsetek palaczy w obu grupach jest taki sam
+
+# test normalności
+
+hist(smog2017$t2m_mean_daily)
+
+# H0: rozkład temperatury jest normalny
+# H1: rozkład temperatury nie jest normalny
+
+# p = 95%
+# alfa = 0.05
+
+shapiro.test(smog2017$t2m_mean_daily)
+# wartość p < alfa = 0.05 - odrzucamy hipotezę zerową
+# rozkład temperatury nie jest normalny
+
+ggplot(smog2017, aes(sample = t2m_mean_daily)) +
+  stat_qq() +
+  stat_qq_line()
+
+# test dla dwóch grup
+# próby zależne
+
+# H0: temp min = temp max
+# H1: temp min != temp max
+
+# p = 95%
+# alfa = 0.05
+
+shapiro.test(smog2017$tmin_daily)
+shapiro.test(smog2017$tmax_daily)
+
+wilcox.test(x = smog2017$tmin_daily, y = smog2017$tmax_daily, paired = TRUE)
+# wartość p < alfa = 0.05 - odrzucamy hipotezę zerową
+# temp min != temp max
+
+t.test(x = smog2017$tmin_daily, y = smog2017$tmax_daily, paired = TRUE)
+
+# skośność i kurtoza
+library(e1071)
+
+skewness(smog2017$t2m_mean_daily)
+kurtosis(smog2017$t2m_mean_daily)
+
+# próby niezależne
+# H0: średnia temp nie różni się w 2 grupach poziomu pm10
+# H1: średnia temp różni się w 2 grupach poziomu pm10
+
+# p = 95%
+# alfa = 0.05
+
+wilcox.test(smog2017$t2m_mean_daily ~ smog2017$pm10_poziom)
+wilcox.test(t2m_mean_daily ~ pm10_poziom, data = smog2017)
+# wartość p < alfa = 0.05 - odrzucamy hipotezę zerową
+# średnia temp różni się w 2 grupach poziomu pm10
+
+ggplot(smog2017, aes(x=t2m_mean_daily, y=pm10_poziom)) +
+  geom_boxplot()
+
+# wiele prób
+ggplot(smog2017, aes(x=t2m_mean_daily, y=kwartal)) +
+  geom_boxplot()
+
+# H0: temperatura we wszystkich kwartałach jest taka sama
+# H1: istnieje co najmniej jedna para kwartałów o różnej temperaturze
+
+# p = 95%
+# alfa = 0.05
+
+kruskal.test(t2m_mean_daily ~ kwartal, data = smog2017)
+# wartość p < alfa = 0.05 - odrzucamy hipotezę zerową
+# istnieje co najmniej jedna para kwartałów o różnej temperaturze
+
+pairwise.wilcox.test(x = smog2017$t2m_mean_daily, g = smog2017$kwartal)
+# wszystkie pary kwartałów różnią się istotnie
+
+# ANOVA
+temp_kw_anova <- aov(t2m_mean_daily ~ kwartal, data = smog2017)
+summary(temp_kw_anova)
+
+TukeyHSD(temp_kw_anova)
+
+# Zadanie
+
+ggplot(smog, aes(x=pm10, y=as.factor(rok))) +
+  geom_boxplot()
+
+kruskal.test(smog$pm10 ~ as.factor(smog$rok))
+pairwise.wilcox.test(x = smog$pm10, g = as.factor(smog$rok))
+
+
+
